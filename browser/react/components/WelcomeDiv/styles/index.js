96f5a6fbc830769ba1e3aa3407828998c2983173
Eliot <eliotpszw@gmail.com>
2017-01-31 15:18:08 -0500

    Some clean up and commenting regarding AFrame

--- browser/react/components/WelcomeDiv/styles/index.js
+++ browser/react/components/WelcomeDiv/styles/index.js
@@ -2,5 +2,9 @@ export default {
   test: {
     marginTop: '5em',
     textAlign: 'center'
+  },
+  container: {
+    height: '100%',
+    width: '100%'
   }
 };
---
+++ browser/react/containers/AFrameContainer.js
@@ -0,0 +1,8 @@
+import SpacePlane from '../components/SpacePlane';
+import { connect } from 'react-redux';
+
+const mapStateToProps = null;
+
+const mapDispatchToProps = null;
+
+export default connect(mapStateToProps, mapDispatchToProps)(SpacePlane);

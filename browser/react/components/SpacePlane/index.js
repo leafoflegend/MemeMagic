d33696715187b0585b60bd304b7373148959c100
Eliot <eliotpszw@gmail.com>
2017-02-01 10:45:51 -0500

    Added commenting to AFrame component

--- browser/react/components/SpacePlane/index.js
+++ browser/react/components/SpacePlane/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import styles from './styles';
 
 export default () => (
+  /*
+    You'll notice that I only use a-entity. This seems to be the solution
+    to many of the race conditions that appear in react/redux playing
+    with A-Frame.
+
+    A-Scene: All A-Frame scenes are rendered in a scene. Consider this the 'view'
+    A-Assets: A way to preload data before a scene begins.
+    A-Entity: Can create all visual elements in a scene.
+    A-Animation: Can control animated effects on entities.
+    A-Camera: Your view portal as the user.
+    A-Sky: A Picture to wrap the entire scene in.
+  */
   <div style={styles.container}>
     <a-scene>
       <a-assets>
